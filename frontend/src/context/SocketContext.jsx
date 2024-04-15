ac9bb3facadae8e94973cdba182e993c37fab895
Bishwash Gurung <bishwashgurung883@gmail.com>
2024-04-15 17:18:07 +0545

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8080", {
+			const socket = io("https://chat-app-project-n2gw.onrender.com/", {
                 query: {
                     userId: authUser._id
                 }
